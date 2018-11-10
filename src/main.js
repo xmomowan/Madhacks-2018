58c4bcabb4a9561d0ac015c94fae2db06390e33d
Ryan Liang <xmomowan@gmail.com>
2018-11-10 14:01:16 -0600

    add arrow and background circle

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import 'p2'
 import Phaser from 'phaser'
 
 import BootState from './states/Boot'
-import SplashState from './states/Splash'
 import GameState from './states/Game'
 
 import config from './config'
@@ -17,7 +16,6 @@ class Game extends Phaser.Game {
     super(width, height, Phaser.CANVAS, 'content', null)
 
     this.state.add('Boot', BootState, false)
-    this.state.add('Splash', SplashState, false)
     this.state.add('Game', GameState, false)
 
     // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,36 +1,64 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Arrow from '../sprites/Arrow'
 import lang from '../lang'
 
 export default class extends Phaser.State {
   init() { }
-  preload() { }
+    preload() {
+        let points = [-10, 3.5,
+                      1, 3.5, 
+                      1, 8,
+                      8, 0,
+                      1, -8,
+                      1, -3.5,
+                      -8, -3.5,
+                      -8, 3.5];
+
+        // make arrow bigger
+        points = points.map((x) => {
+
+            return x * 3;
+        });
+
+        let arrowBluePoly = new Phaser.Polygon(points);
+        let arrowBlueGraphics = new Phaser.Graphics(game, {x: -400, y: -300});
+        arrowBlueGraphics.beginFill(0xFF38ff);
+        arrowBlueGraphics.drawPolygon(points);
+        arrowBlueGraphics.endFill();
+        this.textured = arrowBlueGraphics.generateTexture();
+
+        // Draw circle
+        var graphics = game.add.graphics(game.world.centerX, game.world.centerY);
+        game.stage.backgroundColor = "#CBCEC6";
+
+        //  Our first arc will be a line only
+        graphics.lineStyle(1, 0x353EC4);
+        graphics.beginFill(0x353EC4);
+
+        // graphics.arc(0, 0, 135, game.math.degToRad(0), game.math.degToRad(90), false);
+        graphics.arc(0, 0, 180, Math.PI/2, 3/2*Math.PI, false);
+
+        //  As we wish to draw a 2nd arc on the SAME Graphics object, we need to move the drawing operation
+        // graphics.moveTo(-100, -100);
+
+        //  This will reset the lineStyle
+        graphics.lineStyle(1,0xFF3300);
+
+        //  And this draws a filled arc
+        graphics.beginFill(0xFF3300);
+
+        //  Note the 'true' at the end, this tells it to draw anticlockwise
+        graphics.arc(0, 0, 180, 3/2*Math.PI, Math.PI/2, false);
+        graphics.endFill();
+    }
 
   create() {
-    const bannerText = lang.text('welcome')
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
-
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+      
+      this.arrowBlue = new Arrow(this.game, this.world.centerX, this.world.centerY, this.textured);
+      this.game.add.existing(this.arrowBlue);
   }
 
   render() {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
   }
 }
